34205c6887d4ac6bca39d684c55af98d0c73e518
Kyl Wellman <itsawmna314@gmail.com>
2016-11-06 19:01:16 -0800

    improve visualization

--- www/agility.js
+++ www/agility.js
@@ -1,24 +1,31 @@
 (function(){
     var instruction = 0, interval;
-    var target = document.getElementById("text");
     var speedSlider = document.getElementById("speed");
+    var quadrants = {
+        0: document.getElementById("q1"),
+        1: document.getElementById("q2"),
+        2: document.getElementById("q3"),
+        3: document.getElementById("q4")
+    };
+
     speedSlider.addEventListener("input", setSpeed);
 
     setSpeed();
     next();
 
     function next() {
+        // Clear the previous target
+        quadrants[instruction].classList.remove("target");
         // Between 1 and 3 - number should change every time
         var incr =  Math.floor(Math.random() * 2) + 1;
-        // Instruction stays in [0,3] to keep everyone sane
+        // Set the next target
         instruction = (instruction + incr) % 4;
-        // Add one when we set the input to shift bounds to [1,4]
-        target.innerHTML = (instruction + 1).toString();
+        quadrants[instruction].classList.add("target");
         window.setTimeout(next, interval);
     }
 
     function setSpeed() {
         var speed = speedSlider.value;
-        interval = (speed * 20) + 500;
+        interval = (speed * 10) + 500;
     }
 })();
